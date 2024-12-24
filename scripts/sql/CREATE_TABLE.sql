0c64d4a16e82b91b66f6ab49df0fda6d83933f3d
Carl Gaskill <carl@spacewalklabs.com>
2024-12-24 10:43:57 -0500

    data sanitization working!!!

---
+++ scripts/sql/CREATE_TABLE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "project_truck_stop".masturbation_data (
+  TimePosted VARCHAR(255),
+  Subreddit VARCHAR(255),
+  Comment VARCHAR,
+  Is_NSFW_subreddit bool,
+  CommentID VARCHAR(255)
+);
---
+++ scripts/sql/DROP_TABLE.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS project_truck_stop.masturbation_data CASCADE
